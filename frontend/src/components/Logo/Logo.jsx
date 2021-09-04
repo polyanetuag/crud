7f1c4a05f6f970776582bbe5aa2fdf6cfde405af
Polyane Tuag <pollyportugues@hotmail.com>
2021-09-04 17:21:55 -0300

    creating component logo

---
+++ frontend/src/components/Logo/Logo.jsx
@@ -0,0 +1,8 @@
+import '../Logo/Logo.css'
+import React from 'react'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default props => 
+<aside className="logo">
+  Logo
+</aside>
