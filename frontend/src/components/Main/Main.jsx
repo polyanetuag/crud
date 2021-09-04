99fbe57d94d1f054b8ff5a36e90ce49383051bb7
Polyane Tuag <pollyportugues@hotmail.com>
2021-09-04 17:22:46 -0300

    creating component Main

---
+++ frontend/src/components/Main/Main.jsx
@@ -0,0 +1,12 @@
+import '../Main/Main.css'
+import React from 'react'
+import Header from '../Header/Header'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default props =>
+  <>
+    <Header />
+    <main className="content">
+      Conteúdo
+    </main>
+  </>
