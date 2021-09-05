912b43ff6c7b6c44b167bfb2e086dd5b2d0b4766
Polyane Tuag <pollyportugues@hotmail.com>
2021-09-05 15:56:26 -0300

    component structure logo

--- frontend/src/components/Logo/Logo.jsx
+++ frontend/src/components/Logo/Logo.jsx
@@ -1,8 +1,11 @@
 import '../Logo/Logo.css'
 import React from 'react'
+import logo from '../../assets/imgs/logo.png'
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default props => 
 <aside className="logo">
-  Logo
+  <a href="/" className="logo">
+    <img src={logo} alt="logo" />
+  </a>
 </aside>
