b7bac8330eb877006e67a1c81535f488d3a2f429
Polyane Tuag <pollyportugues@hotmail.com>
2021-09-07 16:46:29 -0300

    Application Home

---
+++ frontend/src/components/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import Main from '../Main/Main'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default props => 
+  <Main icon="home" title="Início" subtitle="Segundo projeto do capítulo de React." >
+      <div className="display-4">Bem Vindo!</div>
+      <hr />
+      <p className="mb-0">Construção de um sistema de cadastro de usuários desenvolvido em React!</p>
+      </Main>
