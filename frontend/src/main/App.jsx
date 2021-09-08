6505a2570e45984a290e490ad60d1391ca5ffdba
Polyane Tuag <pollyportugues@hotmail.com>
2021-09-08 09:18:24 -0300

    implementing routes

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
+import { HashRouter } from 'react-router-dom'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 
 import Logo from '../components/Logo/Logo'
 import Nav from '../components/Nav/Nav'
-import Home from '../components/Home/Home'
 import Footer from '../components/Footer/Footer'
+import Routes from './Routes'
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default props => 
-  <div className="app">
-    <Logo />
-    <Nav />
-    <Home />
-    <Footer />
-  </div>
+  <HashRouter>
+    <div className="app">
+      <Logo />
+      <Nav />
+      <Routes />
+      <Footer />
+    </div>
+  </HashRouter>
