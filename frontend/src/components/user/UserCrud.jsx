d1ca75f1775b50a2b2c1bfb9cda2c3d8ddc67c31
Polyane Tuag <pollyportugues@hotmail.com>
2021-09-09 16:52:44 -0300

    implementing form

--- frontend/src/components/user/UserCrud.jsx
+++ frontend/src/components/user/UserCrud.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-expressions */
 import React, { Component } from "react";
 import Main from '../Main/Main'
 import axios from 'axios'
@@ -33,17 +34,78 @@ export default class UserCrud extends Component {
       })
   }
 
+  //atualizar os campos nome e email
+  updateField(event) {
+    const user = { ...this.state.user}
+    user[event.target.name] = event.target.value
+    this.setState({ user })
+  }
+
   //atualizar o usuário
-  getUpdated(user) {
+  getUpdatedList(user) {
     const list = this.state.list.filter(u => u.id !== user.id)
     list.unshift(user)
     return list
   }
 
+  //renderizando formulário
+  renderForm() {
+    return (
+      <div className="form">
+        <div className="row">
+          <div className="col-12 col-md-6">
+            <div className="form-group">
+              <label>Nome</label>
+              <input 
+                type="text" 
+                className="form-control" 
+                name="name"
+                value={this.state.user.name}
+                onChange={e=> this.updateField(e)}
+                placeholder="Digite o nome..."
+                />
+            </div>
+          </div>
+
+          <div className="col-12 col-md-6">
+            <div className="form-group">
+              <label>Email</label>
+              <input 
+                type="email" 
+                className="form-control" 
+                name="email"
+                value={this.state.email}
+                onChange={e=> this.updateField(e)}
+                placeholder="Digite o email..."
+                />
+            </div>
+          </div>
+        </div>
+
+        <hr />
+        <div className="row">
+          <div className="col-12 d-flex justify-content-end">
+            <button 
+              className="btn btn-primary" 
+              onClick={e => this.save(e)}>
+                Salvar
+            </button>
+
+            <button 
+              className="btn btn-secondary mx-2"
+              onClick={e => this.clear(e)}>
+                Cancelar
+            </button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
   render() {
     return (
       <Main {...headerProps}>
-        Usuário
+        {this.renderForm()}
       </Main>
     )
   }
